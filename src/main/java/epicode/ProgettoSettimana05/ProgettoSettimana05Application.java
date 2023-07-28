e600515a2daf064ea2b1b1a10daac5e490d9025f
lorevis92 <lorenzo.visconti@hotmail.it>
2023-07-28 10:34:34 +0200

    Creazione progetto Spring e collegamento a DB

---
+++ src/main/java/epicode/ProgettoSettimana05/ProgettoSettimana05Application.java
@@ -0,0 +1,13 @@
+package epicode.ProgettoSettimana05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimana05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimana05Application.class, args);
+	}
+
+}
---
+++ src/test/java/epicode/ProgettoSettimana05/ProgettoSettimana05ApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.ProgettoSettimana05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimana05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
